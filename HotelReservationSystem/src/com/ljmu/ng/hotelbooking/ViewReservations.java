19d66f44472406b8f73c208d726a5eac4fc9023a
walldavid@gmail.com <walldavid@gmail.com>
2020-12-04 14:23:51 +0000

    updates to reservation class

--- HotelReservationSystem/src/com/ljmu/ng/hotelbooking/ViewReservations.java
+++ HotelReservationSystem/src/com/ljmu/ng/hotelbooking/ViewReservations.java
@@ -18,28 +18,59 @@ public class ViewReservations {
 	public ViewReservations() {}
 	
 	public static void viewReservations() {
-		System.out.println("You have selected, 'View Room Reservations'");
+		System.out.println("**** View Reservations ****");
 		
-		Scanner emailScanner = new Scanner(System.in);
-		System.out.print("Email address to look up: ");
-		
-		emailAddress = emailScanner.next();
-		
+		Scanner choice = new Scanner(System.in);
+
+		System.out.println("What do you want to view?\n\n1. All Rooms\n2. Only Reserved Rooms\n3. Specific booking by Email Address\n\n ");
+		int ans = choice.nextInt();
+
+		switch(ans) {
+			case 1 : {
+				getAllRoomDetails();
+				break;
+			}
+
+			case 2 : {
+				getAllReservedRoomDetails();
+				break;
+			}
+
+			case 3 : {
+				System.out.print("Email address to look up: ");
+				Scanner emailAddressScanner = new Scanner(System.in);
+				emailAddress = emailAddressScanner.next();
+				getRevervationsByEmailAddress(emailAddress);
+				emailAddressScanner.close();
+				break;
+			}
+		}
+		choice.close();
+
+	}
+
+	private static void getAllRoomDetails() {
+		System.out.println("You have called the getAllRoomDetails method");
+	}
+
+	private static void getAllReservedRoomDetails() {
+		System.out.println("You have called the getAllReservedRoomDetails method");
+	}
+
+
+	private static void getRevervationsByEmailAddress(String emailAddress) {
+
 		List<HotelRoom> hotelRooms = ReadMyFile.getHotelRoomList();
-		
-		System.out.println("Your List of hotel Rooms contains " + hotelRooms.size() + " rooms");
-		
+
 		for(HotelRoom hotelRoom: hotelRooms) {
 			if(hotelRoom.reservationDetail.equalsIgnoreCase(emailAddress)) {
 				System.out.print("Reservation Details for " + emailAddress + " " + hotelRoom.toString() + "]");
 				foundReservation = true;
 			}
 		}
-		
-		if(!foundReservation) { 
-			System.out.println("No reservation found for " + emailAddress); 
+
+		if(!foundReservation) {
+			System.out.println("No reservation found for " + emailAddress);
 		}
-		
-		emailScanner.close();
 	}
 }
