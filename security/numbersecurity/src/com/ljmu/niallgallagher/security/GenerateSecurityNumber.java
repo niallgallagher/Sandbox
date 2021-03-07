e1a74c333602087c871abb69a615f6dc14c5d140
David Wall <walldavid@gmail.com>
2021-03-07 16:33:46 +0000

    adding security folder for java project

---
+++ security/numbersecurity/src/com/ljmu/niallgallagher/security/GenerateSecurityNumber.java
@@ -0,0 +1,10 @@
+package com.ljmu.niallgallagher.security;
+
+public class GenerateSecurityNumber {
+
+    public GenerateSecurityNumber() {}
+
+    public int getSecurityNumber(Long num) {
+        // Do the code to generate the number and return the integer value
+    }
+}
---
+++ security/numbersecurity/src/com/ljmu/niallgallagher/security/Main.java
@@ -0,0 +1,6 @@
+package com.ljmu.niallgallagher.security;
+
+public static void main(String[] args) {
+
+
+}
