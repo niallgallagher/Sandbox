e032e4cfa181f52be59f65d86fd10110be7432c5
Niall Gallagher <niallgallagher2000@gmail.com>
2021-03-30 11:17:34 +0100

    uplaoding txt

--- security/numbersecurity/src/com/ljmu/niallgallagher/security/Main.java
+++ security/numbersecurity/src/com/ljmu/niallgallagher/security/Main.java
@@ -7,22 +7,28 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
+<<<<<<< HEAD
         //System.out.println("How many numbers do you want to generate? ");
-        Scanner scan = new Scanner(System.in);
+    /*    Scanner scan = new Scanner(System.in);
         int numberOfNums = scan.nextInt();
         Random random = new Random();
         StringBuffer sb;
+=======
         System.out.println("What operation do you want to perform?\n");
         System.out.println("1. Generate random unsecure numbers and add to file");
         System.out.println("2. Update existing unsecure file with security digit");
         System.out.println("3. Validate existing secure file");
+>>>>>>> 60026fb961f361bb660d97c559656a61d06a6920
 
         Scanner ans = new Scanner(System.in);
         String userChoice = ans.next();
 
-        NumberFileWriter.writeNumbersToFile(securityNumber);
+<<<<<<< HEAD
+            NumberFileWriter.writeNumbersToFile(securityNumber);
+        */
 
         MyFileReader.readMyFile();
+=======
         switch(userChoice) {
             case "1" :
                 System.out.println("How many numbers do you want to generate? ");
@@ -36,5 +42,6 @@ public class Main {
                 FileOperations.verifyNumberFile();
                 break;
         }
+>>>>>>> 60026fb961f361bb660d97c559656a61d06a6920
     }
 }
