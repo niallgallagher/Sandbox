55160b881beec357e8b9c08a13f3035348a94faf
Niall Gallagher <niallgallagher2000@gmail.com>
2021-07-14 13:45:15 +0100

    adding footballteam code

--- footballteam/src/AddTeam.java
+++ footballteam/src/AddTeam.java
@@ -16,40 +16,64 @@ public class AddTeam {
     public static void includeAFootballTeam() throws IOException {
         do {
             addATeam();
-            System.out.println("\nDo you want to add another football team? 1. Yes 2. No: ");
+            System.out.println("\nDo you want to add another football team? Yes or No: ");
             Scanner sc = new Scanner(System.in);
             addFootballTeam = sc.next();
-        }
 
-        while (addFootballTeam.equalsIgnoreCase("Yes"));
+        } while (addFootballTeam.equalsIgnoreCase("Yes"));
+
         if (addFootballTeam.equalsIgnoreCase("No")){
+            footballClubWriter();
             System.out.println("Goodbye");
             Menu.GenerateMenu();
-           
+
         }
-        addATeam();
+
     }
-        static void addATeam() throws IOException {
 
-        Scanner scan = new Scanner(System.in);
+    static void addATeam() throws IOException {
 
-        //Below I ask the users choice of team.
-        System.out.println("\n***** Add A Football Team *****");
+            Scanner scan = new Scanner(System.in);
 
-        System.out.println("\nPlease Enter Club Name: ");
-        String teamName = scan.nextLine();
-        System.out.println("\nPlease Enter Stadium Name: ");
-        String stadiumName = scan.nextLine();
-        System.out.println("\nPlease Enter Stadium Capacity: ");
-        int capacity = scan.nextInt();
-        System.out.println("\nPlease Enter Team Nickname: ");
-        String nickname = scan.next();
+            //Below I ask the users choice of team.
+            System.out.println("\n***** Add A Football Team *****");
 
-        footballTeam = new FootballTeam(teamName, stadiumName, capacity, nickname);
-        System.out.println(footballTeam.toString());
-       // fWriter.append((CharSequence) footballTeam);
-       // fWriter.flush();
-       // fWriter.close();
+            System.out.println("\nPlease Enter Club Name: ");
+            String teamName = scan.nextLine();
+            System.out.println("\nPlease Enter Stadium Name: ");
+            String stadiumName = scan.nextLine();
+            System.out.println("\nPlease Enter Stadium Capacity: ");
+            int capacity = scan.nextInt();
+            System.out.println("\nPlease Enter Team Nickname: ");
+            String nickname = scan.next();
+
+            footballTeam = new FootballTeam(teamName, stadiumName, capacity, nickname);
+            footballTeam = (FootballTeam) footballTeamList;
+
+        }
+
+    /*private static void footballClubWriter() throws IOException {
+        FileWriter fileWriter = new FileWriter("clubs.txt");
+        for (FootballTeam str : footballTeamList) {
+            fileWriter.write((str + System.lineSeparator()));
+        }
+        fileWriter.close();
+    }*/
+    private static void footballClubWriter() throws IOException {
+        String oldLine = "";
+        String newLine = "";
+        String fileDataString = "";
+        String fileName = "rooms.txt";
+
+        newLine = oldLine.replace("", (CharSequence) footballTeamList);
+        String newFileData = fileDataString.replaceAll(oldLine, newLine);
+        FileWriter writer = new FileWriter(fileName);
+        writer.append(newFileData);
+        writer.flush();
+        writer.close();
+
+    }
+
        // System.out.println("Your team is saved");
           //  for(FootballTeam str: footballTeamList) {
            //     writer.write(str + System.lineSeparator());
@@ -92,6 +116,6 @@ public class AddTeam {
         }*/
     }
 
-}
+
 
 
--- footballteam/src/ViewTeam.java
+++ footballteam/src/ViewTeam.java
@@ -1,8 +1,28 @@
+import java.util.Scanner;
+
 public class ViewTeam {
 
     public ViewTeam() {}
 
     public static void viewFootballTeams() {
         System.out.println("\n**** View Football Teams ****");
+
+        Scanner choice = new Scanner(System.in);
+
+        System.out.println("\nWhat do you want to view?\n\n1. All Clubs\n2. Main Menu");
+        int ans = choice.nextInt();
+        switch(ans) {
+            case 1 : {
+               // getAllRoomDetails();
+                break;
+            }
+
+            case 2 : {
+                Menu.GenerateMenu();
+                break;
+            }
+
     }
+}
+
 }
